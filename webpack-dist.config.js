5a499bfed8518b2de9d93bf4651b514c3d4ace43
dwj <dwj@erongdu.com>
2019-05-15 10:19:48 +0800

    修改

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -100,7 +100,7 @@ module.exports = {
 
      // await publishGhPages();
 
-       调用 Chrome 渲染出 PDF 文件
+      // 调用 Chrome 渲染出 PDF 文件
        const chromePath = findChrome();
        spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
          'https://jessie-d.github.io/resume/' // 这里注意改成你的在线简历的网站
